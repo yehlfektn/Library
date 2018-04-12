f839972ae0ba07ee3d660a65472a555d8e076ab7
yehlfektn <nurdaulet.kenges@nu.edu.kz>
2018-04-13 02:58:53 +0600

    Book Details Activity some changes

--- app/src/main/java/gravityfalls/library/main/BookDetailsActivity.java
+++ app/src/main/java/gravityfalls/library/main/BookDetailsActivity.java
@@ -4,8 +4,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -21,10 +21,14 @@ public class BookDetailsActivity extends AppCompatActivity {
     ImageView imageView;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
- /*   @BindView(R.id.txt_title)
+    @BindView(R.id.txt_title)
     TextView title;
     @BindView(R.id.txt_author)
-    TextView author;*/
+    TextView author;
+    @BindView(R.id.txt_year)
+            TextView year;
+    @BindView(R.id.txt_description)
+            TextView description;
 
     Unbinder unbinder;
     private String TAG = "BookDetailsActivity";
@@ -42,15 +46,15 @@ public class BookDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setTitleTextColor(Color.WHITE);
 
-
         if (getIntent().getExtras() != null) {
             Book book = getIntent().getExtras().getParcelable("data");
             if (book!=null) {
-                Log.e(TAG, "Book: " + book.getAuthor());
-                Glide.with(this).load(book.getImageLink()).into(imageView);
-                /*title.setText(book.getTitle());
-                author.setText(book.getAuthor());*/
                 setTitle(book.getTitle());
+                Glide.with(this).load(book.getImageLink()).into(imageView);
+                title.setText(book.getTitle());
+                author.setText(book.getAuthor());
+                year.setText(book.getYear());
+                description.setText(book.getShort_description());
             }
         }
 

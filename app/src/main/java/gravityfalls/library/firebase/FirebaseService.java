e000f02639f44ba19a2df71a4275f72b6cb6e814
Ilyas <ilyas.turimbetov@nu.edu.kz>
2018-04-14 03:25:33 +0600

    DrawerBuilder Formatting

---
+++ app/src/main/java/gravityfalls/library/firebase/FirebaseService.java
@@ -0,0 +1,26 @@
+package gravityfalls.library.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by 777 on 14.04.2018.
+ */
+
+public class FirebaseService extends FirebaseInstanceIdService {
+    String TAG = "FirebaseService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // sendRegistrationToServer(refreshedToken);
+    }
+}

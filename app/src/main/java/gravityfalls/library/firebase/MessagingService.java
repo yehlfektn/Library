d312acb370d2d4c6695ea9c010d46cc8394f97c4
Ilyas <ilyas.turimbetov@nu.edu.kz>
2018-04-14 04:49:21 +0600

    Notifications Added

--- app/src/main/java/gravityfalls/library/firebase/MessagingService.java
+++ app/src/main/java/gravityfalls/library/firebase/MessagingService.java
@@ -1,9 +1,22 @@
 package gravityfalls.library.firebase;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+import gravityfalls.library.R;
 
 /**
  * Created by 777 on 14.04.2018.
@@ -11,25 +24,59 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
 
+    String ADMIN_CHANNEL_ID = "13";
+
+    NotificationManager notificationManager;
+
     String TAG = "FMessagingService";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //Setting up Notification channels for android O and above
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels();
+        }
+
+        int notificationId = new Random().nextInt(60000);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background) //a resource for your custom small icon
+                .setAutoCancel(true) //dismisses the notification on click
+                .setSound(defaultSoundUri);
+
+
 
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
+            notificationBuilder.setContentTitle(remoteMessage.getData().get("title")) //the "title" value you sent in your notification
+                    .setContentText(remoteMessage.getData().get("message")); //ditto
             Log.wtf(TAG, "Message data payload: " + remoteMessage.getData());
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
+            notificationBuilder.setContentTitle(remoteMessage.getNotification().getTitle()) //the "title" value you sent in your notification
+                    .setContentText(remoteMessage.getNotification().getBody()); //ditto
             Log.wtf(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        notificationManager.notify(notificationId /* ID of notification */, notificationBuilder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(){
+        CharSequence adminChannelName = getString(R.string.notifications_admin_channel_name);
+
+        NotificationChannel adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_LOW);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
     }
 }
